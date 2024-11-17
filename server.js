a57fafd50fd077fc62cca0b86b95b99795338bd3
T-Dharun <dharuneee007@gmail.com>
2024-11-17 15:17:57 +0530

    Added

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express=require('express');
 const app=express();
 const port=3000;
+app.use(express.json());
 app.get('/',(req,res)=>{
     res.send('Hello Eroder!');
+});
+app.get('/home',(req,res)=>{
+    res.send('Hello Home!');
+});
+app.post('/',(req,res)=>{
+    res.send(req.body?.name + 'Hello World!');
 });
 app.listen(port,()=>console.log(`Example app listening on port ${port}!`));
